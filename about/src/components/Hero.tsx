3a3cccc84e0609c41350847e3d8e95ad765d96e7
V. Prescott <victorprescott554@gmail.com>
2026-07-08 17:52:00 +0530

    feat: add writer block

--- about/src/components/Hero.tsx
+++ about/src/components/Hero.tsx
@@ -1,28 +1,30 @@
+import { ArrowDown } from 'lucide-react'
+
 export default function Hero () {
     return (
         <>
-            <div className="title">
+            <header className="title">
                 <h1>Gaurav Mali</h1>
                 <p>Building software, tools, and stories</p>
-            </div>
-            <div className="direct-download-link">
-                <p>
-                    [
-                    <a href="/resume.pdf" download>
-                        Download Resume
-                    </a>
-                    ]
-                </p>   
-            </div>
+                <div className="direct-download-link">
+                    <p>
+                        [
+                        <a href="/resume.pdf" download>
+                            Download Resume
+                        </a>
+                        ]
+                    </p>   
+                </div>
+            </header>
             <div className="se-block">
-                    <h6>
+                    <h6 className="se-head">
                         { 
                             <a href="">
                                 Software Engineer
                             </a>
                         }
                     </h6>
-                    <p>
+                    <p className="se-sub">
                         code
                         design
                         security
@@ -35,6 +37,26 @@ export default function Hero () {
                 <div className="ground-one"></div>
                 <div className="ground-two"></div>
             </div>
+            <div className="wrtr-block">
+                <h6 className="wrtr-head">
+                    [ 
+                        <a href="">
+                            Writer
+                        </a>
+                    ]
+                </h6>
+                <p className="wrtr-sub">
+                    Tech
+                    Fiction
+                    Realism
+                </p>
+            </div>
+            <footer className="hero-foot">
+                    <h6>
+                        Dive
+                    </h6>
+                    <ArrowDown color="#20243D"/>
+            </footer>
         </>
     )
 }
