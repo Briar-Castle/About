6964d206bc23a4ab7460b98c6215018da39ac878
V. Prescott <victorprescott554@gmail.com>
2026-07-09 16:17:00 +0530

    feat: add softeng component

---
+++ about/src/components/Softeng.tsx
@@ -0,0 +1,10 @@
+export default function Softeng () {
+    return (
+        <>
+            <div className="content">
+                <h1>Software Engineer</h1>
+                
+            </div>
+        </>
+    )
+}
