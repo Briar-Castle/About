598eff1ab6914ff1ed2bec59c0612499bfdb4375
V. Prescott <victorprescott554@gmail.com>
2026-08-30 19:17:17 +0530

    feat: add cloud path reference registry

---
+++ about/src/data/clouds_path.tsx
@@ -0,0 +1,13 @@
+export const clouds = {
+    long_cloud: '/assets/clouds/long_cloud.svg',
+    long_cloud_cap: '/assets/clouds/long_cloud_cap.svg',
+    oddball: '/assets/clouds/oddball_cloud.svg',
+    tiny_dough: '/assets/clouds/playful_dough_cloud.svg'
+}
+
+export const cloud_reflection = {
+    long_cloud: '/assets/clouds/reflection/long_cloud.svg',
+    long_cloud_cap: '/assets/clouds/reflection/long_cloud_cap.svg',
+    oddball: '/assets/clouds/reflection/oddball_cloud.svg',
+    tiny_dough: '/assets/clouds/reflection/playful_dough_cloud.svg'
+}
